5595358c28ac67097f4d0311f472755f05fdfb3f
Luc <psyluche@gmail.com>
2021-12-19 22:02:07 -0500

    Initial commit with day 1 part 1 done.

---
+++ src/day_1/mod.rs
@@ -0,0 +1,32 @@
+pub mod day_1 {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+
+    //
+    //Day 1 advent of code solver!
+    //
+    pub fn solve_day_1() -> u32 {
+        let file = File::open("./input_day1.txt").expect("Failed to read input file.");
+        let file = BufReader::new(file);
+        let mut depth_data : Vec<u32> = Vec::new();
+
+        for line in file.lines() {
+            depth_data.push(line.unwrap().trim().parse::<u32>().unwrap());
+        }
+        let mut next_iter = depth_data.iter().peekable();
+        let mut count: u32 = 0;
+        for (_current, &current_reading) in depth_data.iter().enumerate() {
+            next_iter.next();
+            match next_iter.peek() {
+                Some(&&next_reading) => {
+                    if next_reading > current_reading {
+                        count += 1;
+                    }
+                },
+                None => {},
+            }
+        }
+        count
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod day_1;
+
+pub use crate::day_1::day_1::solve_day_1;
+
+fn main() {
+    println!("Number of reading is {}", solve_day_1());
+}
